e6b050f464fd6d258b4e26ab45e372656e7b0f38
Aggietech2842 <zrdpxctyz@outlook.com>
2024-11-19 15:41:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Stel de omgevingstaal in op Nederlands
+export LANG=nl_NL.UTF-8
+export LANGUAGE=nl_NL:en
+export LC_ALL=nl_NL.UTF-8
+
+# Controleer of de taal is geïnstalleerd en installeer indien nodig
+locale-gen nl_NL.UTF-8
+update-locale LANG=nl_NL.UTF-8
+
 # Start de VNC-server
 vncserver :1 -geometry 1280x800 -depth 24
 
